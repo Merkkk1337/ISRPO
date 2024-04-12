0d482286189ec34f5822d5ec8c4888c108589528
Merkkk1337 <merkkek2@gmail.com>
2024-04-12 14:28:53 +0300

    123

---
+++ UDPchat/UDPchat/MainForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace UDPchat
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.listBoxChat = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(12, 28);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(205, 22);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(15, 56);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(102, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(123, 56);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(94, 23);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 119);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(205, 22);
+            this.txtMessage.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Message";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(15, 147);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(102, 23);
+            this.btnSend.TabIndex = 8;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // listBoxChat
+            // 
+            this.listBoxChat.FormattingEnabled = true;
+            this.listBoxChat.ItemHeight = 16;
+            this.listBoxChat.Location = new System.Drawing.Point(223, 9);
+            this.listBoxChat.Name = "listBoxChat";
+            this.listBoxChat.Size = new System.Drawing.Size(565, 196);
+            this.listBoxChat.TabIndex = 9;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 232);
+            this.Controls.Add(this.listBoxChat);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUsername);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox listBoxChat;
+    }
+}
+
---
+++ UDPchat/UDPchat/MainForm.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace UDPchat
+{
+    public partial class MainForm : Form
+    {
+        private UdpClient client;
+        private Thread receivingThread;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Инициализация клиента UDP
+            client = new UdpClient();
+            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            client.EnableBroadcast = true;
+
+            // Запуск потока для приема сообщений
+            receivingThread = new Thread(ReceiveMessages);
+            receivingThread.IsBackground = true;
+        }
+
+        
+
+        private void UpdateChatDisplay(string message)
+        {
+            if (listBoxChat.InvokeRequired)
+            {
+                listBoxChat.Invoke(new Action(() => UpdateChatDisplay(message)));
+            }
+            else
+            {
+                listBoxChat.Items.Add(message);
+                listBoxChat.Refresh(); // Добавьте вызов Refresh после добавления элемента
+            }
+        }
+
+        private int FindAvailablePort()
+        {
+            const int startPort = 11000;
+            const int endPort = 12000; // Вы можете настроить диапазон портов в соответствии с вашими потребностями
+
+            for (int port = startPort; port <= endPort; port++)
+            {
+                try
+                {
+                    var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                    socket.Bind(new IPEndPoint(IPAddress.Any, port));
+                    socket.Close();
+                    return port;
+                }
+                catch (SocketException ex)
+                {
+                    // Проверяем код ошибки, чтобы узнать причину блокировки порта
+                    if (ex.SocketErrorCode == SocketError.AccessDenied)
+                    {
+                        // Порт заблокирован (нет доступа)
+                        // продолжаем поиск следующего порта
+                        continue;
+                    }
+                    else
+                    {
+                        // Возникла другая ошибка, выбрасываем исключение
+                        throw;
+                    }
+                }
+            }
+
+            throw new Exception("No available port found in the specified range.");
+        }
+
+
+        // Объявляем булевую переменную для отслеживания состояния потока
+        private bool isReceivingThreadRunning = false;
+
+        // В методе приема сообщений меняем значение переменной
+        private void ReceiveMessages()
+        {
+
+            try
+            {
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+                while (true)
+                {
+                    byte[] data = client.Receive(ref remoteEP);
+                    string message = Encoding.UTF8.GetString(data);
+                    UpdateChatDisplay(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Получаем имя пользователя
+            string username = txtUsername.Text;
+
+            // Закрываем текущий сокет, если он уже был открыт
+            if (client != null)
+            {
+                client.Close();
+                client.Dispose();
+            }
+
+            // Создаем новый экземпляр UdpClient
+            client = new UdpClient();
+
+            int availablePort = FindAvailablePort();
+            client.Client.Bind(new IPEndPoint(IPAddress.Any, availablePort));
+
+            // Присоединяемся к группе рассылки
+            client.JoinMulticastGroup(IPAddress.Parse("239.255.255.250"));
+
+            // Проверяем, выполняется ли поток приема сообщений
+            if (!isReceivingThreadRunning)
+            {
+                // Если поток не выполняется, мы можем попытаться запустить его
+                receivingThread = new Thread(ReceiveMessages);
+                receivingThread.Start();
+            }
+
+            // Блокируем кнопку "Start", чтобы не было возможности повторного подключения
+            btnStart.Enabled = false;
+
+            MessageBox.Show("Connected to group chat as: " + username);
+        }
+
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            // Получаем имя пользователя
+            string username = txtUsername.Text;
+
+            // Проверяем, что сообщение не пустое
+            if (!string.IsNullOrWhiteSpace(txtMessage.Text))
+            {
+                // Подготавливаем сообщение для отправки в формате "username: сообщение"
+                string message = username + ": " + txtMessage.Text;
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                // Отправляем сообщение всем участникам группы
+                client.Send(data, data.Length, new IPEndPoint(IPAddress.Parse("239.255.255.250"), 11000));
+
+                // Очищаем текстовое поле сообщения
+                txtMessage.Clear();
+            }
+        }
+
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            // Завершаем прием сообщений и покидаем группу рассылки
+            receivingThread.Abort();
+            client.DropMulticastGroup(IPAddress.Parse("239.255.255.250"));
+
+            MessageBox.Show("Disconnected from group chat");
+
+            // Разблокируем кнопку "Start" для возможности повторного подключения
+            btnStart.Enabled = true;
+        }
+
+    }
+}
